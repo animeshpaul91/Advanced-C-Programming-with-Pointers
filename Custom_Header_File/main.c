87ac02beb358a50ce2cb8e45aa990734c8fc4221
Animesh Paul <42789828+animeshpaul91@users.noreply.github.com>
2019-09-13 00:20:23 -0400

    Update main.c

--- Custom_Header_File/main.c
+++ Custom_Header_File/main.c
@@ -7,12 +7,12 @@ int main(int argc, char **argv)
 	int n,i,*arr,sum;
 	float avg;
 	printf("\nEnter the number of elements in the array\n");
-    scanf("%d", &n);
-    arr = (int *)malloc(n * sizeof(int));
-    for(i=0;i<n;i++)
-    { printf("\nEnter A[%d] = ", i+1);
-      scanf("%d", arr+i);
-    }
+	scanf("%d", &n);
+	arr = (int *)malloc(n * sizeof(int));
+	for(i=0;i<n;i++)
+	{	printf("\nEnter A[%d] = ", i+1);
+	 	scanf("%d", arr+i);
+	}
 	multiple_return(arr, n, &sum, &avg);
 	printf("\nThe Sum is = %d and the average is = %f\n", sum, avg);
 	return (0);
